7d26c994ac1532afe5865fa45df843d7e990ab55
jensieg <jensieg@umich.edu>
2021-04-18 13:52:16 -0400

    list of artists created

--- MusicMadness.py
+++ MusicMadness.py
@@ -1,1 +1,30 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+import unittest
+
+#Names of Partners: Ponette Rubio and Jenny Siegel
+
+#Create a list of 100 artists using the Billboard Artist 100 Website
+def billboard_list():
+    url = 'https://www.billboard.com/charts/artist-100'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    lst = []
+    data = soup.find('div', class_ = 'chart-details')
+    rows = data.find_all('div', class_  = "chart-list-item")
+    
+    for row in rows:
+        art = row.find_all('span', class_ = "chart-list-item__title-text")
+        for a in art:
+            artist= a.text.strip()
+            lst.append(artist)
+    return lst
+    
+#throughout the rest of the project, simply call billboard_list() to access the list of artists
+
+#this goes at bottom of page
+billboard_list()
+
+
 
