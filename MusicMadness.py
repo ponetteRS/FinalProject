dc4ffd560eb4c53095873eb04b49f215a425e844
jensieg <jensieg@umich.edu>
2021-04-20 20:49:30 -0400

    more edits

--- MusicMadness.py
+++ MusicMadness.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 import re
 import requests
 import unittest
+import json
 
 #Names of Partners: Ponette Rubio and Jenny Siegel
 
-#Create a list of 100 artists using the Billboard Artist 100 Website
+#Creates a list of 100 artists using the Billboard Artist 100 Website
 def billboard_list():
     url = 'https://www.billboard.com/charts/artist-100'
     r = requests.get(url)
@@ -21,10 +22,34 @@ def billboard_list():
             lst.append(artist)
     return lst
 
-list_of_artists = billboard_list()   
-#throughout the rest of the project, list_of_artists is the global variable for all 100 artists 
+#Creates JSON object of up to 25 songs per artist
+def iTunes_songs(artist):
+    url= 'https://itunes.apple.com/search'
+    param= {'term': artist, 'entity': 'song', 'limit': '25'}  
+    iTunes= requests.get(url, params= param)
+    data= json.loads(iTunes.text)
+    return data
+    
+#Saves song data to a database
+#def songs_table(cur, conn):
+
+#Creates JSON object of up to 10 albums per artist
+def iTunes_albums(artist):
+    url= 'https://itunes.apple.com/search'
+    param= {'term': artist, 'entity': 'album', 'limit': '10'}  
+    iTunes= requests.get(url, params= param)
+    data= json.loads(iTunes.text)
+    return data
+    
+#Saves album data to a database
 
 
 
+#throughout the rest of the project, list_of_artists is the global variable for all 100 artists
+#def main(): 
+#    list_of_artists = billboard_list() 
+#    for artist in list_of_artists:
+#        songs = iTunes_songs(artist)
+#        albums = iTunes_albums(artist)
 
 
