0c70ac7c01cece68e3a2badb62cf25270011f003
Ponette <ponette@umich.edu>
2021-04-26 23:00:39 -0400

    Genius work/comments removed.

--- MusicMadness.py
+++ MusicMadness.py
@@ -6,9 +6,9 @@ import sqlite3
 import os
 import matplotlib.pyplot as plt
 
-# Project Name: Music Madness
-# Names: Ponette Rubio
-# Names of Partners: Ponette Rubio and Jenny Siegel
+# Project artist: Music Madness
+# artists: Ponette Rubio
+# artists of Partners: Ponette Rubio and Jenny Siegel
 
 #Creates a list of 100 artists using the Billboard Artist 100 Website
 def billboard_list():
@@ -35,7 +35,7 @@ def iTunes_songs(artist):
 
 def setUpDatabase():
     path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+ "finaldatabase.db")
+    conn = sqlite3.connect(path+'/'+ "1finaldatabase.db")
     cur = conn.cursor()
     return cur, conn
 
@@ -49,31 +49,16 @@ def iTunes_albums(artist):
     iTunes= requests.get(url, params= param)
     data= json.loads(iTunes.text)
     return data
-    
 #Saves album data to a database
 
 list_of_artists = billboard_list()   
 #throughout the rest of the project, list_of_artists is the global variable for all 100 artists 
-
-
-# converts artist name to the url ending in the Genius url format
-# ex Ariana-grande, Lady-gaga, Rihanna, etc.
-def reformat(artist):
-    reformatted_artist = artist
-    #Makes first char upper case and rest of string lower case if two words
-    reformatted_artist = reformatted_artist[0].upper() + reformatted_artist[1:].lower()
-    #adds the - that url has
-    reformatted_artist = reformatted_artist.replace(' ', '-')
-    
-    return reformatted_artist
     
 
-# Get the artist with most likes from Genius website from artists from billboard
-# This will be done by collecting the number of likes that artist got on their page/artist description
-def artist_likes():
-    # setUpDatabase() #temp until main merge
-    # cur.execute("CREATE TABLE IF NOT EXISTS artistsLikes (name TEXT PRIMARY KEY, likes INTEGER)")
-    # cur.execute("SELECT likes FROM artistsLikes")
+# Get the artist on Billboard 100
+def artist_weeks():
+    cur.execute("CREATE TABLE IF NOT EXISTS artistWeeks (artist TEXT PRIMARY KEY, num_weeks INTEGER)")
+    cur.execute("SELECT num_weeks FROM artistWeeks")
     
     url = 'https://www.billboard.com/charts/artist-100'
     r = requests.get(url)
@@ -89,31 +74,15 @@ def artist_likes():
          for ar in artist_row:
              artist= ar.text.strip()
              temp = stats_row[2].text
-             weeks = temp.split()[0] #number of weeks on the billboard
-             d[artist] = weeks #quick testing
-    return d
-   
-    # for artist in artists_list:
-    #     try: 
-    #         artist = str(reformat(artist))
-    #         temp_url = url + artist #adds artist name to the url ending in the Genius url format
-    #         # print(temp_url) #urls go to right website
-    #         r = requests.get(url)
-    #         soup = BeautifulSoup(r.text, 'html.parser')
-    #         data = soup.find('div', class_= 'voting-total square_button square_button--transparent voting-total--positive')
-    #         # print(data)
-        
-    #         likes_d[artist] = data #dict to see changes
-    #         # will import like information into the database
-    #         # cur.execute("INSERT OR IGNORE INTO artistsLikes (name, likes) VALUES (?, ?)", (name, likes))
-    #     except:
-    #         likes_d[artist] = 0 #artists not on Genius get no likes
-    # # conn.commit()
-    # return likes_d
+             num_weeks = temp.split()[0] #number of weeks on the billboard
+             cur.execute("INSERT OR IGNORE INTO artistWeeks (artist, num_weeks) VALUES (?, ?)", (artist, weeks))
+            #  d[artist] = num_weeks #quick testing
+    # return d
+    conn.commit()
 
 # returns list of top 10 artists
 def top_ten():
-    cur.execute('SELECT text FROM artistLikes ORDER BY likes DESC') #none should self filter out
+    cur.execute('SELECT text FROM artistweeks ORDER BY weeks DESC') #none should self filter out
     all_artists = cur.fetchall()
     top = []
     for artist in all_artists[:10]:
@@ -124,20 +93,19 @@ def top_ten():
 def graphics():
     pass
 
+# Tests for artist_weeks()
+# print(artist_weeks())
 
-# tests for reformat(artist)
-# reformatted_artist = 'Ariana Grande'
-# # reformatted_artist = 'Rihanna'
-# reformatted_artist = reformatted_artist[0].upper() + reformatted_artist[1:].lower()
-# reformatted_artist = reformatted_artist.replace(' ', '-')
-# print(reformatted_artist)
-
-# Tests for rartist_likes()
-print(artist_likes())
-                            ######## calls to be added to main ########
-
-# setUpDatabase() #run when fixed scrape issue
-# artist_likes()
+# artist_weeks()
 # print(top_ten()) 
 
+def main():
+    #creating fileartist
+    path = os.path.dirname(os.path.realpath(__file__))
+    cur, conn = setUpDatabase()
+    # artist_weeks()
+    # print(top_ten())
 
+
+if __name__ == "__main__":
+    main()
